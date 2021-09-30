6ca7805a4124ba3451c1fb825c46ee8217768727
Komarov Artem <dethloony@yandex.ru>
2021-10-01 01:35:02 +0300

    Выполнение 1ого задания. Добавление переменных, алерта и консольлог.

---
+++ script.js
@@ -0,0 +1,20 @@
+let title;
+let screens;
+let screenPrice;
+let rollback;
+let fullPrice;
+let adaptive;
+
+let alertMessage = "Сегодня ты стал лучше чем вчера, а завтра будешь лучше чем сегодня.";
+let consoleMessage = "Долги по заданию - это не моя проблема, это проблема завтрашнего меня)))";
+
+let alertFunction = () => {
+    alert(alertMessage);
+};
+
+let consoleFunction = () => {
+    console.log(consoleMessage);
+};
+
+alertFunction();
+consoleFunction();
