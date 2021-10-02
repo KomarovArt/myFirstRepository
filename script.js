c1241357613bb02c94bfff0653b08f51e36084d0
Komarov Artem <dethloony@yandex.ru>
2021-10-02 12:25:39 +0300

    Выполнение первого урока, первого задания.

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = "Типы данных, операторы, методы и свойства";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 15;
+let rollback = Math.random(0, 100);
+let fullPrice = 150000;
+let adaptive = true;
 
-let alertMessage = "Сегодня ты стал лучше чем вчера, а завтра будешь лучше чем сегодня.";
-let consoleMessage = "Долги по заданию - это не моя проблема, это проблема завтрашнего меня)))";
+//Вывести в консоль тип данных значений переменных title, fullPrice, adaptive;
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
 
-let alertFunction = () => {
-    alert(alertMessage);
-};
+//Вывести в консоль длину строки screens
+console.log(screens.length);
 
-let consoleFunction = () => {
-    console.log(consoleMessage);
-};
+//Вывести в консоль “Стоимость верстки экранов (screenPrice) рублей/ долларов/гривен/юани” и “Стоимость разработки сайта (fullPrice) рублей/ долларов/гривен/юани”
+console.log("Стоимость верстки экранов " + screenPrice + " долларов");
+console.log("Стоимость разработки сайта " + (fullPrice) + " рублей");
 
-alertFunction();
-consoleFunction();
+//Привести строку screens к нижнему регистру и разбить строку на массив, вывести массив в консоль
+console.log(screens.toLocaleLowerCase().split(", "));
+
+//Вывести в консоль Процент отката посреднику за работу (fullPrice * (rollback/100))
+let percentageForWork = fullPrice * (rollback / 100);
+console.log(Math.round(percentageForWork));
