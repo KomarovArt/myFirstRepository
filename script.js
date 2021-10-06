794a6f8297d69a0eaaf3e971fa91f69b0873a9d9
Artem Komarov <dethloony@yandex.ru>
2021-10-06 17:03:55 +0300

    Выполнение 4ого урока

--- script.js
+++ script.js
@@ -11,56 +11,78 @@ let service2 = prompt("Какой дополнительный тип услуг
 let servicePrice2 = prompt("Сколько это будет стоить?");
 let fullPrice = Number(screenPrice) + Number(servicePrice1) + Number(servicePrice2);
 let servicePercentPrice;
+let screensArr = screens.split(", ");
 
 
 
 //Объявить функцию getAllServicePrices. Функция возвращает сумму всех дополнительных услуг. Результат сохраняем в переменную allServicePrices. Тип - function expression
 const getAllServicePrices = function () {
-    return servicePrice1 + servicePrice2;
+     return servicePrice1 + servicePrice2;
 };
 
 /*Объявить функцию getFullPrice. 
 Функция возвращает сумму стоимости верстки и стоимости дополнительных услуг 
 (screenPrice + allServicePrices). Результат сохраняем в переменную fullPrice. Тип - function declaration*/
 function getFullPrice() {
-    return screenPrice + getAllServicePrices();
+     return screenPrice + getAllServicePrices();
 }
 
 /*Объявить функцию getTitle. Функция возвращает title меняя его таким образом:
  первый символ с большой буквы, остальные с маленькой". Учесть вариант что строка 
  может начинаться с пустых символов. " КаЛьКулятор Верстки"*/
 const getTitle = function () {
-    let resulttitle = title.trim();
-    resulttitle = resulttitle.toLowerCase();
-    resulttitle = resulttitle[0].toUpperCase() + resulttitle.substring(1);
-    return resulttitle;
+     let resulttitle = title.trim();
+     resulttitle = resulttitle.toLowerCase();
+     resulttitle = resulttitle[0].toUpperCase() + resulttitle.substring(1);
+     return resulttitle;
 };
 
 /* Объявить функцию getServicePercentPrices. Функция возвращает итоговую стоимость
  за вычетом процента отката. Результат сохраняем в переменную
   servicePercentPrice (итоговая стоимость минус сумма отката)*/
 const getServicePercentPrices = function () {
-    return Math.ceil(getFullPrice() - (getFullPrice() * (rollback / 100)));
+     return Math.ceil(getFullPrice() - (getFullPrice() * (rollback / 100)));
 };
 
+//Вывести в консоль строку из переменной screens в виде массива
+
+console.log(screensArr);
 console.log(getTitle());
 
 
 
-// if (fullPrice >= 30000) {
-//     console.log("Даем скидку в 10%");
-// } else if (fullPrice >= 15000 && fullPrice < 30000) {
-//     console.log("Даем скидку в 5%");
-// } else if (fullPrice >= 0 && fullPrice < 15000) {
-//     console.log("Скидка не предусмотрена");
-// } else {
-//     console.log("Что то пошло не так");
-// }
 
+/*Почистить консоль логи и добавить недостающие, должны остаться:
+- вызовы функции showTypeOf
+- вывод строки с типами экранов для разработки screens
+- сообщение о скидке пользователю (вызовы функции getRollbackMessage)
+- стоимость за вычетом процента отката посреднику (вызовы функции getServicePercentPrices)*/
+
+const showTypeOf = function (variable) {
+     console.log(variable, typeof variable);
+};
+
+const getRollbackMessage = function () {
+     if (fullPrice >= 30000) {
+          console.log("Даем скидку в 10%");
+     } else if (fullPrice >= 15000 && fullPrice < 30000) {
+          console.log("Даем скидку в 5%");
+     } else if (fullPrice >= 0 && fullPrice < 15000) {
+          console.log("Скидка не предусмотрена");
+     } else {
+          console.log("Что то пошло не так");
+     }
+};
+
+showTypeOf(title);
+showTypeOf(fullPrice);
+showTypeOf(adaptive);
+console.log(screens);
+
+
+getRollbackMessage();
 /*
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
+
 console.log(screens.length);
 console.log("Стоимость верстки экранов " + screenPrice + " долларов");
 console.log("Стоимость разработки сайта " + (fullPrice) + " рублей");
