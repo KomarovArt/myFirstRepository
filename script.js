bdb1c209331484da36f471d0a1affe9814bef7a3
Komarov Artem <dethloony@yandex.ru>
2021-10-10 16:28:58 +0300

    Исправлен баг с кнопкой отмена

--- script.js
+++ script.js
@@ -12,32 +12,34 @@ const game = function () {
     let goGame = confirm("Играем?");
 
     const gameResult = function () {
-        if (goGame) {
-            if (isNumber(numHidden)) {
-                if (numHidden !== randNumber) {
-                    numHidden = Number(prompt(confirm("Угадай число от 1 до 100")));
-                    if (numHidden == false) {
-                        alert("Игра окончена");
-                        game();
-                    } else if (numHidden < randNumber) {
-                        console.log(numHidden);
-                        alert("Заданное число больше!");
-                    } else if (numHidden > randNumber) {
-                        alert("Загаданное число меньше!");
-                    }
+
+        if (isNumber(numHidden)) {
+            if (numHidden !== randNumber) {
+                numHidden = Number(prompt(confirm("Угадай число от 1 до 100")));
+                if (numHidden == false) {
+                    alert("Игра окончена");
+                } else if (numHidden < randNumber) {
+                    console.log(numHidden);
+                    alert("Заданное число больше!");
                     gameResult();
-                } else {
-                    alert("Поздравляю, Вы угадали!!!");
+                } else if (numHidden > randNumber) {
+                    alert("Загаданное число меньше!");
+                    gameResult();
                 }
+
             } else {
-                alert("Введи число!");
-                numHidden = 0;
-                gameResult();
+                alert("Поздравляю, Вы угадали!!!");
             }
         } else {
-            alert("Игра окончена");
+            alert("Введи число!");
+            numHidden = 0;
+            gameResult();
         }
     };
-    gameResult();
+    if (goGame) {
+        gameResult();
+    } else {
+        alert("Игра окончена");
+    }
 };
 game();
