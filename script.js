6f8e6e49f03feff9c86eef19662779dad847a2d3
Komarov Artem <dethloony@yandex.ru>
2021-10-10 16:40:14 +0300

    Исправлен баг с кнопкой отмена

--- script.js
+++ script.js
@@ -12,34 +12,33 @@ const game = function () {
     let goGame = confirm("Играем?");
 
     const gameResult = function () {
+        if (goGame) {
+            if (isNumber(numHidden)) {
+                if (numHidden !== randNumber) {
+                    numHidden = Number(prompt(confirm("Угадай число от 1 до 100")));
+                    if (numHidden == false) {
+                        alert("Игра окончена");
+                    } else if (numHidden < randNumber) {
+                        console.log(numHidden);
+                        alert("Заданное число больше!");
+                        gameResult();
+                    } else if (numHidden > randNumber) {
+                        alert("Загаданное число меньше!");
+                        gameResult();
+                    }
 
-        if (isNumber(numHidden)) {
-            if (numHidden !== randNumber) {
-                numHidden = Number(prompt(confirm("Угадай число от 1 до 100")));
-                if (numHidden == false) {
-                    alert("Игра окончена");
-                } else if (numHidden < randNumber) {
-                    console.log(numHidden);
-                    alert("Заданное число больше!");
-                    gameResult();
-                } else if (numHidden > randNumber) {
-                    alert("Загаданное число меньше!");
-                    gameResult();
+                } else {
+                    alert("Поздравляю, Вы угадали!!!");
                 }
-
             } else {
-                alert("Поздравляю, Вы угадали!!!");
+                alert("Введи число!");
+                numHidden = 0;
+                gameResult();
             }
         } else {
-            alert("Введи число!");
-            numHidden = 0;
-            gameResult();
+            alert("Игра окончена");
         }
     };
-    if (goGame) {
-        gameResult();
-    } else {
-        alert("Игра окончена");
-    }
+    gameResult();
 };
 game();
