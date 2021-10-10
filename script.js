9ebd482523f14467cc693ece038d6e9e532f685f
Komarov Artem <dethloony@yandex.ru>
2021-10-10 13:43:17 +0300

    Выполнение 6 урока основного задания

--- script.js
+++ script.js
@@ -1,100 +1,36 @@
 'use strict';
 
-let rollback;
-let title;
-let screens;
-let screenPrice;
-let adaptive;
-let service1;
-let service2;
-let fullPrice;
-let allServicePrices;
-let screensArr;
-let resulttitle;
-let servicePrice;
+let numHidden = 0;
+let goGame = confirm("Играем?");
 
 const isNumber = function (num) {
     return !isNaN(parseFloat(num)) && isFinite(num);
-}
-
-const asking = function () {
-    rollback = Math.round((Math.random()) * 100);
-    title = prompt("Как называется ваш проект?", "Калькулятор верстки");
-    screens = prompt("Какие типы экранов нужно разобрать?", "Простые, Сложные, Интерактивные");
-
-    do {
-        screenPrice = Number(prompt("Сколько будет стоить данная работа?", "12000"));
-    } while (!isNumber(screenPrice));
-
-    adaptive = confirm("Нужен ли адаптив на сайте?");
 };
 
-const getAllServicePrices = function () {
-    let sum = 0;
-
-    for (let i = 0; i < 2; i++) {
-        servicePrice = "";
-        if (i === 0) {
-            service1 = prompt("Какой дополнительный тип услуги нужен?");
-        } else if (i === 1) {
-            service2 = prompt("Какой дополнительный тип услуги нужен?");
+const game = function () {
+    let randNumber = Math.round(Math.random() * 100);
+    const gameResult = function () {
+        if (goGame) {
+            if (isNumber(numHidden)) {
+                if (numHidden !== randNumber) {
+                    numHidden = Number(prompt("Угадай число от 1 до 100", "88"));
+                    if (numHidden < randNumber) {
+                        alert("Заданное число больше!");
+                    } else if (numHidden > randNumber) {
+                        alert("Загаданное число меньше!");
+                    }
+                    gameResult();
+                } else {
+                    alert("Поздравляю, Вы угадали!!!");
+                }
+            } else {
+                alert("Введи число!");
+                gameResult();
+            }
+        } else {
+            alert("Игра окончена");
         }
-
-        while (!isNumber(servicePrice)) {
-            servicePrice = Number(prompt("Сколько это будет стоить?"));
-        }
-        sum += servicePrice;
-
-        //дебаг работы, проверка на входящий тип
-        console.log(typeof (servicePrice));
-    }
-    return sum;
-    // return servicePrice1 + servicePrice2;
+    };
+    gameResult();
 };
-
-function getFullPrice() {
-    return Number(screenPrice) + Number(allServicePrices);
-}
-
-const getTitle = function () {
-    title = title.trim().toLowerCase();
-    return title[0].toUpperCase() + title.substring(1);
-};
-
-const getServicePercentPrices = function () {
-    return Math.ceil(getFullPrice() - (getFullPrice() * (rollback / 100)));
-};
-
-const showTypeOf = function (variable) {
-    console.log(variable, typeof variable);
-};
-
-const getRollbackMessage = function () {
-    if (fullPrice >= 30000) {
-        console.log("Даем скидку в 10%");
-    } else if (fullPrice >= 15000 && fullPrice < 30000) {
-        console.log("Даем скидку в 5%");
-    } else if (fullPrice >= 0 && fullPrice < 15000) {
-        console.log("Скидка не предусмотрена");
-    } else {
-        console.log("Что то пошло не так");
-    }
-};
-
-asking();
-
-allServicePrices = getAllServicePrices();
-fullPrice = getFullPrice();
-title = getTitle();
-
-showTypeOf(title);
-showTypeOf(fullPrice);
-showTypeOf(adaptive);
-getRollbackMessage();
-
-screensArr = screens.split(", ");
-console.log(allServicePrices);
-console.log(getServicePercentPrices());
-console.log(screens);
-console.log(screensArr);
-console.log(title);
+game();
